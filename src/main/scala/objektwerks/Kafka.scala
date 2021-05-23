2cee878237a4d7f3c489ec285a5be649aa7d6ade
objektwerks <objektwerks@runbox.com>
2021-05-23 12:03:46 -0400

    refactored Kafka

--- src/main/scala/objektwerks/Kafka.scala
+++ src/main/scala/objektwerks/Kafka.scala
@@ -3,7 +3,6 @@ package objektwerks
 import io.github.embeddedkafka._
 
 import java.time.Duration
-
 import java.util.Properties
 
 import org.slf4j.LoggerFactory
@@ -15,17 +14,37 @@ import org.apache.kafka.clients.producer.KafkaProducer
 
 import scala.jdk.CollectionConverters._
 
-object Kafka {
-  def apply(): Kafka = new Kafka()
-}
-
-class Kafka extends EmbeddedKafka {
+object Kafka extends EmbeddedKafka {
   val logger = LoggerFactory.getLogger(getClass)
   implicit val serializer = new StringSerializer()
   implicit val deserializer = new StringDeserializer()
-  implicit val config = EmbeddedKafkaConfig.defaultConfig
+
+  def apply(): Kafka = new Kafka()
+
+  def createTopic(name: String): Unit = createCustomTopic(name).toOption.get
+
+  def product(topic: String, key: String, value: String): Unit = {
+    val producer = new KafkaProducer[String, String](new Properties())
+    val record = new ProducerRecord[String, String](topic, key, value)
+    producer.send(record)
+    logger.info(s"*** producer send: $record")
+    ()
+  }
+ 
+  def consume(topic: String): List[ConsumerRecord[String, String]] = {
+    val consumer = new KafkaConsumer[String, String](new Properties())
+    consumer.subscribe( List(topic).asJava )
+    val records = consumer.poll( Duration.ofMillis(3000L) ).asScala.toList
+    logger.info(s"*** comsumer poll: ${records.foreach(println)}")
+    records
+  }
+}
+
+class Kafka {
+  import Kafka.logger 
 
   logger.info("*** embedded kafka starting ...")
+  implicit val config = EmbeddedKafkaConfig.defaultConfig
   val kafka = EmbeddedKafka.start()
   logger.info("*** embedded kafka started")
 
@@ -34,20 +53,5 @@ class Kafka extends EmbeddedKafka {
     kafka.stop(false)
     logger.info("*** embedded kafka stopped")
     ()
-  }
-
-  def createTopic(name: String): Unit = createCustomTopic(name).toOption.get
-
-  def product(topic: String, key: String, value: String): Unit = {
-    val producer = new KafkaProducer[String, String](new Properties())
-    val record = new ProducerRecord[String, String](topic, key, value)
-    producer.send(record)
-    ()
-  }
- 
-  def consume(topic: String): List[ConsumerRecord[String, String]] = {
-    val consumer = new KafkaConsumer[String, String](new Properties())
-    consumer.subscribe( List(topic).asJava )
-    consumer.poll( Duration.ofMillis(3000L) ).asScala.toList
   }
 }
