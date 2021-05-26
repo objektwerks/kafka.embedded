8525011273ac679dce8922c80420da37a4447549
objektwerks <objektwerks@runbox.com>
2021-05-26 09:33:08 -0400

    refactored simulation report

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -21,19 +21,21 @@ class Store(conf: Config) {
 
   def addDeviceReading(reading: DeviceReading): Int = run( query[DeviceReading].insert(lift(reading)).returningGenerated(_.id) )
 
-  def listDevices(): Seq[Device] = run( query[Device] )
+  def listDevices: Seq[Device] = run( query[Device] )
 
-  def listDeviceReadings(): Seq[DeviceReading] = run( query[DeviceReading] )
+  def listDeviceReadings: Seq[DeviceReading] = run( query[DeviceReading] )
 
   def buildReport: Array[String] = {
-    val devices = listDevices()
-    val readings = listDeviceReadings()
-    var valueAvg = readings.map(reading => reading.value).fold(0.0)(_ + _) / readings.size
-    valueAvg = new DecimalFormat("#.##").format(valueAvg).toDouble
+    val devices = listDevices
     val builder = mutable.ArrayBuilder.make[String]
-    builder += s"*** Number of devices: ${devices.size}"
-    builder += s"*** Number of device readings: ${readings.size}"
-    builder += s"*** Average value(temp) of device readings: $valueAvg"
+    builder += s"*** Device Report [${devices.size}]"
+    for( (deviceId, readings) <- listDeviceReadings.groupBy(_.deviceId) ) {
+      devices.find(device => device.id == deviceId).foreach {
+        val formatter = new DecimalFormat("#.##")
+        val valueAvg = formatter.format( readings.map(reading => reading.value).fold(0.0)(_ + _) / readings.size )
+        device => builder += s"*** Device: ${device.name} Readings: ${readings.size} Average: $valueAvg"
+      }
+    }
     builder.result()
   }
 }
